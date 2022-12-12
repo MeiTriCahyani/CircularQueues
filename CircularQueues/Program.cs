857c8229015639c7744d6b5df83756ae8900b7c4
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-12-12 17:45:06 +0700

    add

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -91,6 +91,41 @@ namespace CircularQueues
                 Console.WriteLine("\nQueue is empty");
                 return;
             }
-            
+            Console.WriteLine("\n Element in the queue are ...................\n");
+            if (FRONT_position <= REAR_position)
+            {
+                //travers the queue till the last element present in an array
+
+                while (FRONT_position<=REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                //traverses the queue till the last position of the array 
+                while (FRONT_position<= max -1)
+                {
+                    Console.Write(queue_array[FRONT_position] + " ");
+                    FRONT_position++;
+                }
+                //set the FRONT position to the first element of the array 
+
+                FRONT_position = 0;
+
+                //traverse the array till the last element present in the queue
+
+                while(FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + " ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+        }
+        
+       
     }
 }
